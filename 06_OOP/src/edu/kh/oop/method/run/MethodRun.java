13a5c876503c5cdfd61b545a3f3863706cc52b22
yhj860411 <yhj860411@gmail.com>
2025-09-23 17:32:33 +0900

    9/23 학원실습

---
+++ 06_OOP/src/edu/kh/oop/method/run/MethodRun.java
@@ -0,0 +1,13 @@
+package edu.kh.oop.method.run;
+
+import edu.kh.oop.method.model.service.MethodExample;
+
+public class MethodRun {
+
+	public static void main(String[] args) {
+		MethodExample ex = new MethodExample();
+		//ex.method1();
+
+	}
+
+}
